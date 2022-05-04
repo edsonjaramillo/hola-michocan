6efe1aaa4c554d121d8f4eaab62b75cb4736fca6
edsonjaramillo <edsonajaramillo@gmail.com>
2022-05-03 20:09:05 -0500

    added props to cta

--- components/home/CTA.tsx
+++ components/home/CTA.tsx
@@ -1,35 +1,37 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-const CTA = () => {
-  return (
-    <div className='responsive-width cta'>
-      <div className='cta__grid'>
-        <div className='cta__header'>
-          <h1 className='cta__title'>Best Mechanic in Town!!</h1>
-          <span className='cta__subtitle'>Come on in an get the best service in town!</span>
-          <div className='cta__buttongroup'>
-            <Link href='/menu'>
-              <a
-                title='Link to services section'
-                aria-label='Link to services section'
-                className='cta__primarybutton'>
-                View Menu
-              </a>
-            </Link>
-          </div>
+interface CTAProps {
+  src: string;
+  alt: string;
+}
+
+const CTA = ({ src, alt }: CTAProps) => (
+  <header className='cta'>
+    <div className='cta__content'>
+      <div className='cta__contentgrid responsive-width-cta'>
+        <h1 className='cta__header'>Your favorite neighborhood homemade ice cream shop!</h1>
+        <span className='cta__subheader'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, ipsa!
+        </span>
+        <div className='cta__buttongrid'>
+          <Link href='/menu'>
+            <a className='cta__button cta--primary'>View Menu</a>
+          </Link>
+          <a
+            rel='noopener nofollow noreferrer external'
+            target='_blank'
+            href='https://www.google.com/maps/place/Hola+Michoac%C3%A1n+ice+cream+and+coffee+shop/@34.3210944,-86.312755,17z/data=!3m1!4b1!4m5!3m4!1s0x8889f919acd21945:0xb5a3288ef5ab0a37!8m2!3d34.32109!4d-86.310561'
+            className='cta__button cta--secondary'>
+            Visit Us
+          </a>
         </div>
-        <picture className='cta__image'>
-          <Image
-            src='https://picsum.photos/id/350/500/500'
-            layout='fill'
-            alt='picture of a dog'
-            priority={true}
-          />
-        </picture>
       </div>
     </div>
-  );
-};
+    <div className='cta__image'>
+      <Image src={src} layout='fill' objectFit='cover' alt={alt} quality={35} priority />
+    </div>
+  </header>
+);
 
 export default CTA;
