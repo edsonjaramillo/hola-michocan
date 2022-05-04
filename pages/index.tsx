ba974e33b6cf573d62cacb93b7bd864d9cb4dc38
edsonjaramillo <edsonajaramillo@gmail.com>
2022-05-03 20:36:29 -0500

    added home components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,38 @@
-import type { NextPage } from 'next';
-import { CTA, Section } from '@/components/index';
+import { GetServerSideProps } from 'next';
+import { CTA, RestaurantJSONLD, AboutUs, FeaturedProducts } from '@/components/index';
+import { graphCMSClient } from '@/lib/graphcms/client';
+import { getFeaturedProducts, getProductsJSONLD } from '@/lib/graphcms/queries';
+import { ProductType } from '@/lib/graphcms/types';
 
-const Home: NextPage = () => (
+interface HomepageProps {
+  menuProducts: ProductType[];
+  featuredProducts: ProductType[];
+}
+
+const Home = ({ menuProducts, featuredProducts }: HomepageProps) => (
   <>
-    <h1 className='responsive-width min-height'>Under Construction</h1>
+    <RestaurantJSONLD products={menuProducts} />
+    <CTA src='https://picsum.photos/id/1060/1920/1080' alt='coffee being brewed' />
+    <AboutUs src='https://picsum.photos/id/513/1920/1080' alt='coffee being brewed' />
+    <FeaturedProducts products={featuredProducts} />
   </>
 );
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const { products: menuProducts } = await graphCMSClient.request(getProductsJSONLD);
+  const { products: featuredProducts } = await graphCMSClient.request(getFeaturedProducts);
+
+  return {
+    props: { menuProducts, featuredProducts },
+  };
+};
+
+// export const getStaticProps: GetStaticProps = async () => {
+// const { products: menuProducts } = await graphCMSClient.request(getProductsJSONLD);
+
+//   return {
+// props: { menuProducts },
+//   };
+// };
 
 export default Home;
