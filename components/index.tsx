8f5e8a9fa0af9cb620d0e5a7263487edee01d012
edsonjaramillo <edsonajaramillo@gmail.com>
2022-05-03 20:08:05 -0500

    added components

--- components/index.tsx
+++ components/index.tsx
@@ -1,4 +1,3 @@
-// Shared
 export { default as Navbar } from '@/components/shared/Navbar';
 export { default as Section } from '@/components/shared/Section';
 export { default as Footer } from '@/components/shared/Footer';
@@ -6,9 +5,14 @@ export { default as HeadOpenGraph } from '@/components/shared/HeadOpenGraph';
 export { default as BlurImage } from '@/components/shared/BlurImage';
 
 export { default as CTA } from '@/components/home/CTA';
+export { default as AboutUs } from '@/components/home/AboutUs';
+export { default as FeaturedProducts } from '@/components/home/FeaturedProducts';
 
 export { default as ProductGridItem } from '@/components/menu/ProductGridItem';
 export { default as ProductJSONLD } from '@/components/menu/ProductJSONLD';
+export { default as RestaurantJSONLD } from '@/components/menu/RestaurantJSONLD';
+export { default as Recommendations } from '@/components/menu/Recommendations';
+export { default as Options } from '@/components/menu/Options';
 
 export { CustomInput as CustomInput } from '@/components/contact/FormInputs';
 export { TextFieldInput as TextFieldInput } from '@/components/contact/FormInputs';
