c0c2484f78b4f194a3b63f1f6832863c17cf9507
edsonjaramillo <edsonajaramillo@gmail.com>
2022-05-09 19:42:18 -0500

    added external tags

--- components/home/AboutUs.tsx
+++ components/home/AboutUs.tsx
@@ -18,6 +18,8 @@ const AboutUs = ({ src, alt }: AboutUsProps) => (
         error commodi at laborum perferendis.
       </p>
       <a
+      rel='noopener nofollow noreferrer external'
+      target='_blank'
         href='https://www.google.com/maps/place/Hola+Michoac%C3%A1n+ice+cream+and+coffee+shop/@34.3210944,-86.312755,17z/data=!3m1!4b1!4m5!3m4!1s0x8889f919acd21945:0xb5a3288ef5ab0a37!8m2!3d34.32109!4d-86.310561'
         className='aboutus__link'>
         Come Visit Us!
